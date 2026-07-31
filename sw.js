4df0086539d17a267139eeec165d78fff96e0997
lukasz8423-code <lukasz8423@gmail.com>
2026-07-31 14:55:15 +0200

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "wariat-ai-v1";
+
+const FILES = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", event => {
+
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(FILES))
+);
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+caches.match(event.request)
+.then(response => response || fetch(event.request))
+);
+
+});
